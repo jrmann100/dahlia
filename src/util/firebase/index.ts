f6d409b766eb64b8e880e7af43c365936b586cd7
Jordan Mann <jordaloo@gmail.com>
2023-04-30 11:01:05 -0400

    a little bit of firebase 🔥

---
+++ src/util/firebase/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBzCA07j7O9zqRPS1jnaucH3vCGlpmYF38',
+  authDomain: 'dali-link.firebaseapp.com',
+  projectId: 'dali-link',
+  storageBucket: 'dali-link.appspot.com',
+  messagingSenderId: '1078229731902',
+  appId: '1:1078229731902:web:32a9f3df68275d719adf6f',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export default app;
