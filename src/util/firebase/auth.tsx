9a8f45fb0c77e897d31a4f5a7d69a2ba44707a24
Jordan Mann <jordaloo@gmail.com>
2023-05-01 01:01:51 -0400

    solve auth state issues by checking isLoadingAuth

--- src/util/firebase/auth.tsx
+++ src/util/firebase/auth.tsx
@@ -1,3 +1,4 @@
+import 'util/firebase/config';
 import { getAuth, onAuthStateChanged, type User } from '@firebase/auth';
 import {
   createContext,
@@ -7,36 +8,51 @@ import {
   useContext,
   type PropsWithChildren,
 } from 'react';
-import { Navigate, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import snack from 'util/notify';
 
 interface AuthContextValue {
-  user: User | null;
+  user: User | null | undefined;
 }
 
 const AuthContext = createContext<AuthContextValue>({
   user: null,
 });
 
+export const auth = getAuth();
+
 export const AuthContextProvider: FC<PropsWithChildren> = (props) => {
-  const [user, setUser] = useState<User | null>(null);
-  const auth = getAuth();
-  useEffect(() => onAuthStateChanged(auth, setUser, snack.catch), [auth]);
+  const [user, setUser] = useState<User | null | undefined>(undefined);
+  useEffect(() => onAuthStateChanged(auth, setUser, snack.catch), []);
   return <AuthContext.Provider value={{ user }} {...props} />;
 };
 
-export const useAuth = (): AuthContextValue & { isAuthenticated: boolean } => {
+export const useAuth = (): AuthContextValue & {
+  isAuthenticated: boolean;
+  isLoadingAuth: boolean;
+} => {
   const { user, ...context } = useContext(AuthContext);
-  return { user, isAuthenticated: user !== null, ...context };
+  return {
+    user,
+    isAuthenticated: user !== null && user !== undefined,
+    isLoadingAuth: user === undefined,
+    ...context,
+  };
 };
 
 export const Protect: FC<
   PropsWithChildren<{ enabled: boolean; path: string }>
-> = ({ children, path, enabled = true, ...props }) => {
-  const { isAuthenticated } = useAuth();
-  return !enabled || isAuthenticated ? (
-    <Route path={path} {...props} />
-  ) : (
-    <Navigate to="/login" state={{ next: path }} replace />
+> = ({ children, path, enabled = true }) => {
+  const { isAuthenticated, isLoadingAuth } = useAuth();
+  return (
+    <>
+      {!enabled || isAuthenticated ? (
+        children
+      ) : isLoadingAuth ? (
+        <>Logging in&hellip; {/* todo: replace with skel */}</>
+      ) : (
+        <Navigate to="/login" state={{ next: path }} replace />
+      )}
+    </>
   );
 };
