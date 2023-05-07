6f216d0312a28dd4b91f84c8a9fe00ffbab2060d
Jordan Mann <jordaloo@gmail.com>
2023-05-07 10:34:20 -0400

    add log out button

--- src/components/Pages/Me.tsx
+++ src/components/Pages/Me.tsx
@@ -1,4 +1,18 @@
+import { Button } from '@mui/material';
 import { type FC } from 'react';
+import { auth } from 'util/firebase/auth';
+import snack from 'util/notify';
 
-const Me: FC = () => <>your profile</>;
+const Me: FC = () => (
+  <>
+    your profile
+    <Button
+      onClick={() => {
+        auth.signOut().catch(snack.catch);
+      }}
+    >
+      log out
+    </Button>
+  </>
+);
 export default Me;
