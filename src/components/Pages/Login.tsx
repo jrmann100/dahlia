1e6f9976f791980cee61635f4af4043cb7e71e62
Jordan Mann <jordaloo@gmail.com>
2023-05-10 22:36:45 -0400

    fix login logic - enter from redirect or load

--- src/components/Pages/Login.tsx
+++ src/components/Pages/Login.tsx
@@ -1,25 +1,64 @@
-import { useEffect, type FC } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, type FC, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import {
   GoogleAuthProvider,
   getRedirectResult,
   signInWithRedirect,
 } from 'firebase/auth';
-import { auth } from 'util/firebase/auth';
+import { auth, useAuth } from 'util/firebase/auth';
 import snack from 'util/notify';
+import pages from 'components/pages/pages';
+import { Button } from '@mui/material';
+import Flexbox from 'components/common/Flexbox';
 
 const Login: FC = () => {
-  const { state } = useLocation();
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { isAuthenticated, isLoadingAuth } = useAuth();
+  const [canContinue, setCanContinue] = useState(false);
+
   useEffect(() => {
+    const next = searchParams.get('next');
+    if (isLoadingAuth) {
+      // wait until we know if logged in
+      return;
+    }
+    // if logged in already
+    if (isAuthenticated) {
+      navigate(next === 'login' || next === null ? pages.home.path : next);
+      return;
+    }
     getRedirectResult(auth)
-      .then(async (result) => {
-        result === null
-          ? await signInWithRedirect(auth, new GoogleAuthProvider())
-          : navigate(state?.path ?? '/home');
+      .then((result) => {
+        if (result === null) {
+          // go to google
+          console.log('can continue...');
+          setCanContinue(true);
+        }
       })
       .catch(snack.catch);
-  }, [navigate, state?.path]);
-  return <>You should be redirected momentarily...</>;
+  }, [isAuthenticated, isLoadingAuth, navigate, searchParams, setSearchParams]);
+  return isLoadingAuth ? (
+    <>Loading&hellip;</>
+  ) : (
+    <Flexbox centering style={{ width: '100%', height: '100%' }}>
+      <Button
+        variant="contained"
+        size="large"
+        disabled={!canContinue}
+        onClick={() => {
+          signInWithRedirect(
+            auth,
+            new GoogleAuthProvider().setCustomParameters({
+              hd: 'dali.dartmouth.edu',
+              prompt: 'consent',
+            })
+          ).catch(snack.catch);
+        }}
+      >
+        Log in
+      </Button>
+    </Flexbox>
+  );
 };
 export default Login;
