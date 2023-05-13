df6dbd563485f15c1fdda4a33594fef254558434
Jordan Mann <jordaloo@gmail.com>
2023-05-13 13:08:30 -0400

    add sx'y spreads. yum yum

---
+++ src/util/spreads.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/prefer-default-export
+const spreads = {
+  flex: { display: 'flex' },
+  resetLink: { textDecoration: 'none', color: 'initial' },
+  centering: { alignItems: 'center', justifyContent: 'center' },
+  full: { width: '100%', height: '100%' },
+  fill: { flexGrow: 1 },
+};
+
+export default spreads;
