eb0ac5aa60c5be3ed369abe46366328904cac4a6
Jordan Mann <jordaloo@gmail.com>
2023-05-13 13:16:19 -0400

    update app to dahlia-web

--- src/util/firebase/config.ts
+++ src/util/firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBVZaa9YT2o0pfuvb5-C3gmTKx00kAfRuY',
-  authDomain: 'dahlia-23208.firebaseapp.com',
-  projectId: 'dahlia-23208',
-  storageBucket: 'dahlia-23208.appspot.com',
-  messagingSenderId: '407922669447',
-  appId: '1:407922669447:web:54ed9f3602fe3532f18522',
+  apiKey: 'AIzaSyCMr_0cot-dRNwfosX2-8-xRvsRyMlT5e4',
+  authDomain: 'dahlia-web.firebaseapp.com',
+  projectId: 'dahlia-web',
+  storageBucket: 'dahlia-web.appspot.com',
+  messagingSenderId: '941777279941',
+  appId: '1:941777279941:web:fa722d71409839d3e809ac',
 };
 
 // Initialize Firebase
